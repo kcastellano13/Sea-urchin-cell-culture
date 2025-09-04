d239aff3f3a6716503f489cb7c55dd97100e25f9
Kate Castellano <48929055+kcastellano13@users.noreply.github.com>
2025-09-04 10:26:46 -0400

    Delete 03_mRNAseq_timescale directory
    Remove old scripts (to be replaced with updated scripts)

--- 03_mRNAseq_timescale/04_hisat_index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Make the L. variegatus 3.0 genome into a hisat2 index
-#K. Castellano
-module load histat2/v2.2.1
-
-hisat2-build GCA_018143015.1_Lvar_3.0_genomic.fna Lvar3.0_hisat2index
-
